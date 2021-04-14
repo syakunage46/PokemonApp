509051da4562b0795e6768189c10df99e2517331
syakunage46 <syakunage46+github@gmail.com>
2021-04-14 14:56:35 +0900

    appのfluxコンポーネントを削除

--- app/src/main/java/com/example/myapplication/flux/Dispatcher.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.flux
-
-import kotlinx.coroutines.flow.Flow
-
-interface Dispatcher<ActionType, StateType> {
-    val actionFlow: Flow<ActionType>
-    val alterFlow: Flow<Alter<StateType>>
-    fun dispose()
-}
--- app/src/main/java/com/example/myapplication/flux/Store.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.flux
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.Flow
-
-abstract class Store<StateType>(private val alterFlow: Flow<Alter<StateType>>): ViewModel() {
-    abstract val state: LiveData<StateType>
-}
