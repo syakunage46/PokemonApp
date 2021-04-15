e65d9b6546e71851d7db8761094c105e4d6d0d5c
syakunage46 <syakunage46+github@gmail.com>
2021-04-15 17:17:27 +0900

    動作確認コードの削除

--- core/src/main/java/com/example/core/state/State.kt
+++ core/src/main/java/com/example/core/state/State.kt
@@ -25,37 +25,3 @@ class StateBuilder(val mutableStateElement: MutableStateElements){
         mutableStateElement[T::class] = element
     }
 }
-
-data class TestData(val number: Int)
-data class TestElement(val text: String): StateElement
-data class TestElement2(val testData: TestData): StateElement
-
-fun main() {
-    val testElement = TestElement("test")
-    val testElement2 = TestElement2(TestData(2))
-
-    val state = State(mapOf(
-        TestElement::class to testElement
-    ))
-
-    val nextState = state.shift {
-        element(prevOrDefault(testElement2).copy(
-            testData = TestData(2)
-        ))
-    }
-
-    nextState.stateElements.forEach {
-        val printText = it.value::class.simpleName + ": " + when(val value = it.value){
-            is TestElement -> {
-                value.text
-            }
-            is TestElement2 -> {
-                value.testData.number.toString()
-            }
-            else -> {
-                it.value.toString()
-            }
-        }
-        println(printText)
-    }
-}
