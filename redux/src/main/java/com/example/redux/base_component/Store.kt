1df256db6accc4a94b192d1a7e45b8eac85cc381
syakunage46 <syakunage46+github@gmail.com>
2021-04-28 10:16:27 +0900

    MutableStateFlowのemitとtryEmitはvalueを内部で呼び出しているからvalueで統一するのがベターらしい

--- redux/src/main/java/com/example/redux/base_component/Store.kt
+++ redux/src/main/java/com/example/redux/base_component/Store.kt
@@ -3,17 +3,18 @@ package com.example.redux.base_component
 import com.example.core.state.State
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 interface StoreInterface {
-    val stateFlow: Flow<State>
+    val state: StateFlow<State>
     fun dispatch(action: Action)
 }
 
 class Store(private val reducerBus: ReducerBusInterface): StoreInterface {
-    private val _stateFlow = MutableStateFlow(State(emptyMap()))
-    override val stateFlow = _stateFlow
+    private val _state = MutableStateFlow(State(emptyMap()))
+    override val state = _state
 
     override fun dispatch(action: Action) {
-        TODO("現在のStateとActionからReducerを呼び出してStateを更新")
+        _state.value = reducerBus.reduce(state.value, action)
     }
 }
