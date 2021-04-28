88dd3cca3fcf2d88ab83b1e473fac23115064adf
syakunage46 <syakunage46+github@gmail.com>
2021-04-28 15:40:50 +0900

    ディスパッチャーにミドルウェアマネージャーを組み込み

--- redux/src/main/java/com/example/redux/base_component/Dispatcher.kt
+++ redux/src/main/java/com/example/redux/base_component/Dispatcher.kt
@@ -1,11 +1,13 @@
 package com.example.redux.base_component
 
+import com.example.redux.middleware.ReduxMiddlewareManagerInterface
+
 interface DispatcherInterface {
     fun dispatch(action: Action)
 }
 
-class Dispatcher(private val store: StoreInterface): DispatcherInterface {
+class Dispatcher(private val store: StoreInterface, private val middlewareManager: ReduxMiddlewareManagerInterface): DispatcherInterface {
     override fun dispatch(action: Action) {
-        store.dispatch(action)
+        middlewareManager.dispatch(store, action)
     }
 }
