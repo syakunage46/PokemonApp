28ca934dd4926202e5fbe42f24797e8ff3071256
syakunage46 <syakunage46+github@gmail.com>
2021-04-28 15:52:17 +0900

    PokemonReducerの追加

--- redux/src/main/java/com/example/redux/pokemon/PokemonAction.kt
+++ redux/src/main/java/com/example/redux/pokemon/PokemonAction.kt
@@ -1,10 +1,11 @@
 package com.example.redux.pokemon
 
 import com.example.core.pokemon.PokemonData
+import com.example.redux.base_component.Action
 
-interface PokemonAction {
+interface PokemonAction: Action {
     class GetList(val payload: List<PokemonData>): PokemonAction
     class AddList(val payload: List<PokemonData>): PokemonAction
-    class StartLoading(): PokemonAction
+    class InLoading: PokemonAction
     class Failed(val payload: Throwable): PokemonAction
 }
--- redux/src/main/java/com/example/redux/pokemon/PokemonReducer.kt
+++ redux/src/main/java/com/example/redux/pokemon/PokemonReducer.kt
@@ -1,9 +1,29 @@
 package com.example.redux.pokemon
 
-//class PokemonReducer: ReducerInterface {
-//    override fun reduce(state: State, action: Action): State {
-//        TODO("Not yet implemented")
-//    }
-//
-//}
+import com.example.core.pokemon.PokemonStateElement
+import com.example.core.state.State
+import com.example.redux.base_component.Action
+import com.example.redux.base_component.ElementReducerInterface
+
+class PokemonReducer: ElementReducerInterface<PokemonStateElement> {
+    override fun reduce(state: State, action: Action): PokemonStateElement {
+        val element = state[PokemonStateElement::class] ?: PokemonStateElement(emptyList(), false, null)
+        return when(action) {
+            is PokemonAction.InLoading -> {
+                element.copy(isLoading = true)
+            }
+            is PokemonAction.Failed -> {
+                element.copy(isLoading = false, error = action.payload)
+            }
+            is PokemonAction.GetList -> {
+                element.copy(pokemonDataList = action.payload, isLoading = false)
+            }
+            is PokemonAction.AddList -> {
+                element.copy(pokemonDataList = element.pokemonDataList + action.payload, isLoading = false)
+            }
+            else -> element
+        }
+    }
+
+}
 
