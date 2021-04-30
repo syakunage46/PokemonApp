7bc925a7799123cad4cce9165cb6cf37d4341e5f
syakunage46 <syakunage46+github@gmail.com>
2021-04-30 16:39:18 +0900

    リポジトリ動作確認用コード削除

--- core/src/main/java/com/example/core/repository/Repository.kt
+++ core/src/main/java/com/example/core/repository/Repository.kt
@@ -1,37 +1,9 @@
 package com.example.core.repository
 
-import kotlinx.coroutines.runBlocking
-
 abstract class Repository {
     suspend fun<T> fetch(query: RepositoryQuery<T>): Result<T> {
         return handle(query) as? Result<T> ?: Result.Failure(Exception("${this::class.simpleName}に${query::class.simpleName}でクエリすることができません"))
     }
 
     internal abstract suspend fun<T> handle(query: RepositoryQuery<T>): Result<*>?
-}
-
-class TestQuery : RepositoryQuery<String> {
-    val test: String = "test"
-}
-
-class TestRepository: Repository() {
-    override suspend fun<T> handle(query: RepositoryQuery<T>): Result<*>? {
-        return when(query) {
-            is TestQuery -> query.answer("Success")
-            else -> null
-        }
-    }
-}
-
-
-fun main() {
-    val repository = TestRepository()
-    runBlocking {
-        val test = repository.fetch(TestQuery())
-        print(test)
-        test.onSuccess {
-            print(it)
-        }
-    }
-
 }
